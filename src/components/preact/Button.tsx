18abd34162a55bf8400798e3c8b5fb9f4c4719e9
Julian Visser <julian.visser@husqvarnagroup.com>
2025-07-30 17:15:58 +0200

    Added CartStore

--- src/components/preact/Button.tsx
+++ src/components/preact/Button.tsx
@@ -1,12 +1,13 @@
 interface ButtonProps {
   title: string;
+  onClick: () => void;
 }
 
-function Button({ title }: ButtonProps) {
+function Button({ title, onClick }: ButtonProps) {
   return (
     <button
       className="inline-block cursor-pointer rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white"
-      onClick={() => console.log('Button clicked')}
+      onClick={onClick}
     >
       {title}
     </button>
