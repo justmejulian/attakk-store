f352a04b1d832cebcd20a3d6cfff1b55edee994b
Julian Visser <julian.visser@husqvarnagroup.com>
2025-07-30 21:43:51 +0200

    display products in cart

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,8 +1,8 @@
 import { useStore } from '@nanostores/preact';
 
 import { cartItems } from '@stores/cartStore';
-
 import useIsHydrated from '@utils/hooks/useIsHydrated';
+import { getProductById } from '@content/products';
 
 import CartItem from './CartItem';
 
@@ -13,12 +13,27 @@ function Cart() {
   if (!isHydrated) return null;
 
   return (
-    <div class="mt-8">
+    <div class="mt-8 w-full max-w-2xl">
       {Object.keys($cartItems).length ? (
-        <ul class="space-y-4">
-          {Object.values($cartItems).map((cartItem) => (
-            <CartItem key={cartItem.id} id={cartItem.id} />
-          ))}
+        <ul class="w-full space-y-4">
+          {Object.values($cartItems).map((cartItem) => {
+            try {
+              const product = getProductById(cartItem.id);
+
+              return (
+                <CartItem
+                  key={cartItem.id}
+                  quantity={cartItem.quantity}
+                  product={product}
+                />
+              );
+            } catch (error) {
+              console.error('Error fetching product:', error);
+              // Todo: clear the cart item if product not found
+              // maybe do all of this in the CartItem
+              return 'Something went wrong while fetching the product.';
+            }
+          })}
         </ul>
       ) : (
         <p>Your cart is empty!</p>
