686d7f26e2c95957511a72b1af1cd122f6b5c0d5
Julian Visser <julian.visser@husqvarnagroup.com>
2025-07-30 16:03:45 +0200

    Added ProductCollection

--- src/components/preact/Button.tsx
+++ src/components/preact/Button.tsx
@@ -5,7 +5,7 @@ interface ButtonProps {
 function Button({ title }: ButtonProps) {
   return (
     <button
-      className="inline-block rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white focus:ring-3 focus:outline-hidden"
+      className="inline-block cursor-pointer rounded-sm border border-indigo-600 px-12 py-3 text-sm font-medium text-indigo-600 hover:bg-indigo-600 hover:text-white"
       onClick={() => console.log('Button clicked')}
     >
       {title}
