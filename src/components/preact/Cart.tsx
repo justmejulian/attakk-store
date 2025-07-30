77c016eef7ef33161d28470ccdd843dff35bd6b0
Julian Visser <julian.visser@husqvarnagroup.com>
2025-07-30 17:58:56 +0200

    presist cart to localstorage

--- src/components/preact/Cart.tsx
+++ src/components/preact/Cart.tsx
@@ -2,23 +2,30 @@ import { useStore } from '@nanostores/preact';
 
 import { cartItems } from '@stores/cartStore';
 
+import { useEffect, useState } from 'preact/hooks';
+
 function Cart() {
+  const [isHydrated, setIsHydrated] = useState(false);
   const $cartItems = useStore(cartItems);
 
-  if (Object.values($cartItems).length) {
-    return (
-      <ul>
-        {Object.values($cartItems).map((cartItem) => (
-          <li>
-            <h3>{cartItem.id}</h3>
-            <p>Quantity: {cartItem.quantity}</p>
-          </li>
-        ))}
-      </ul>
-    );
-  }
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
 
-  return <p>Your cart is empty!</p>;
+  if (!isHydrated) return null;
+
+  return Object.keys($cartItems).length ? (
+    <ul>
+      {Object.values($cartItems).map((cartItem) => (
+        <li>
+          <h3>{cartItem.id}</h3>
+          <p>Quantity: {cartItem.quantity}</p>
+        </li>
+      ))}
+    </ul>
+  ) : (
+    <p>Your cart is empty!</p>
+  );
 }
 
 export default Cart;
