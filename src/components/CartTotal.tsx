42fead8ae664c9e4af89ef770ec59a0a9972e390
Julian Visser <julian.visser@husqvarnagroup.com>
2025-07-31 20:38:46 +0200

    Added total component

---
+++ src/components/CartTotal.tsx
@@ -0,0 +1,26 @@
+import { useStore } from '@nanostores/preact';
+import { cartItems } from '@stores/cartStore';
+import useIsHydrated from '@utils/hooks/useIsHydrated';
+import { getProductById } from '@content/products';
+
+function CartTotal() {
+  const isHydrated = useIsHydrated();
+  const $cartItems = useStore(cartItems);
+  const total = Object.values($cartItems).reduce((acc, item) => {
+    const product = getProductById(item.id);
+    return acc + product.price * item.quantity;
+  }, 0);
+
+  if (!isHydrated) return null;
+
+  return (
+    <dl className="text-tertiary space-y-0.5 text-sm">
+      <div className="flex justify-between !text-base font-medium">
+        <dt>Total</dt>
+        <dd>{total.toFixed(2)} CHF</dd>
+      </div>
+    </dl>
+  );
+}
+
+export default CartTotal;
