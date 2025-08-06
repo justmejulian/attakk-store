a87eccccda11a8e9585f29bd5c729037002922a5
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-08-06 16:22:58 +0200

    fix input for keys. cause focus to jump on selection

--- src/components/SizeButtons.tsx
+++ src/components/SizeButtons.tsx
@@ -1,36 +1,41 @@
 import { useStore } from '@nanostores/preact';
 import { selectedSize } from '@stores/cartStore';
-
 interface Props {
+  id: string;
   sizes: string[];
 }
 
-export default function SizeButtons({ sizes }: Props) {
+export default function SizeButtons({ id, sizes }: Props) {
   const $selectedSize = useStore(selectedSize);
   return (
     <fieldset class="mb-3 flex gap-1">
       <legend class="sr-only">Delivery</legend>
 
-      {sizes.map((size) => (
-        <div class="flex items-center" key={size}>
-          <label
-            for={`Size${size}`}
-            class={`hover:bg-primary hover:text-secondary cursor-pointer border-2 px-4 py-2 ${$selectedSize === size && 'bg-primary text-secondary'}`}
-          >
-            <p class="text-gray-700">{size}</p>
+      {sizes.map((size) => {
+        const key = `Size-${size}-${id}`;
+        return (
+          <div class="flex items-center" key={size}>
+            <label
+              for={key}
+              class={`hover:bg-primary hover:text-secondary cursor-pointer border-2 px-4 py-2 ${$selectedSize === size && 'bg-primary text-secondary'}`}
+            >
+              <p class="text-gray-700">{size}</p>
 
-            <input
-              type="radio"
-              name="SizeOption"
-              value={size}
-              id={`Size${size}`}
-              class="sr-only"
-              checked={$selectedSize === size}
-              onChange={() => selectedSize.set(size)}
-            />
-          </label>
-        </div>
-      ))}
+              <input
+                type="radio"
+                name="SizeOption"
+                value={size}
+                id={key}
+                class="sr-only"
+                checked={$selectedSize === size}
+                onChange={() => {
+                  selectedSize.set(size);
+                }}
+              />
+            </label>
+          </div>
+        );
+      })}
     </fieldset>
   );
 }
