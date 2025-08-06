62b482b8ddeb07c82ea56699e7a3092fdfc1e84d
Julian Visser <12615757+justmejulian@users.noreply.github.com>
2025-08-06 16:22:59 +0200

    created the return page

---
+++ src/components/PaymentStatus.tsx
@@ -0,0 +1,99 @@
+import { Fragment } from 'preact';
+import { actions } from 'astro:actions';
+import useIsHydrated from '@utils/hooks/useIsHydrated';
+
+async function fetchSessionStatus(sessionId?: string | null) {
+  if (!sessionId) {
+    throw new Error('Session ID is required to fetch session status');
+  }
+  const { data, error } = await actions.getSessionStatus({ sessionId });
+  if (error) {
+    throw new Error(`Error fetching session status: ${error.message}`);
+  }
+  return data;
+}
+
+import { useState, useEffect } from 'preact/hooks';
+
+export default function PaymentStatus() {
+  // todo: fix
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const [sessionStatus, setSessionStatus] = useState<any>();
+  const [loading, setLoading] = useState(true);
+  const isHydrated = useIsHydrated();
+
+  if (!isHydrated) {
+    return null;
+  }
+
+  useEffect(() => {
+    const fetchStatus = async () => {
+      const searchParams = new URLSearchParams(window?.location?.search);
+      const sessionId = searchParams?.get('session_id');
+      setLoading(true);
+      const result = await fetchSessionStatus(sessionId);
+      setSessionStatus(result);
+      setLoading(false);
+    };
+    fetchStatus();
+  }, []);
+
+  if (loading) {
+    return (
+      <Fragment>
+        <p class="text-center">Loading Payment Status...</p>
+      </Fragment>
+    );
+  }
+
+  if (!sessionStatus) {
+    return (
+      <Fragment>
+        <h1 class="mb-4 text-center">Payment Status</h1>
+        <p class="text-center text-red-500">No payment status available.</p>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Fragment>
+      <h1 class="mb-4 text-center">Payment Successful</h1>
+      <div class="mb-4 flex w-full items-center justify-center">
+        <svg
+          data-id="4"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="h-16 w-16 text-green-500"
+        >
+          <circle cx="12" cy="12" r="10"></circle>
+          <path d="m9 12 2 2 4-4"></path>
+        </svg>
+      </div>
+
+      <p class="border-b border-gray-200 pb-2 text-center">
+        Amount Paid: {(sessionStatus.total / 100).toFixed(2)} CHF
+      </p>
+
+      <p class="mt-4 text-center">
+        Thank you for your purchase!
+        <br />
+        Your payment has been successfully processed.
+      </p>
+
+      <p class="mt-4">
+        Email confirmation has been sent to{' '}
+        <span class="text-tertiary font-thin underline">
+          {sessionStatus.customerEmail}
+        </span>
+        .
+      </p>
+    </Fragment>
+  );
+}
