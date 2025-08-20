5afc52100b17fbd18a64bcc7a188db5ade61626d
Julian Visser <12615757+justmejulian@users.noreply.github.coml>
2025-08-20 15:14:04 +0200

    added way to imort current drop form stripe

---
+++ scripts/create-products.ts
@@ -0,0 +1,110 @@
+import fs from 'fs';
+
+import { getProducts, getPrices } from '@utils/stripe';
+
+import type { Product } from '@stripe/stripe-js';
+
+type SizeProduct = {
+  stripeProductId: string;
+};
+
+export type Sex = 'Male' | 'Female' | 'Unisex';
+
+function mapSexToPrices(prices) {
+  return prices.reduce((acc: Record<string, Product[]>, price) => {
+    if (!price.metadata) {
+      console.warn('Price metadata is missing:', price);
+      return acc;
+    }
+    const sex = price.metadata.Sex || 'Unisex';
+    if (acc[sex]) {
+      acc[sex].push(price);
+    } else {
+      acc[sex] = [price];
+    }
+    return acc;
+  }, {});
+}
+
+async function createProductObject(
+  product: Product,
+  sex,
+  prices,
+): Promise<Product> {
+  return {
+    id: `${product.id}-${sex.toLowerCase()}`,
+    title: product.name,
+    description: product.description,
+    imageUrls: product.images,
+    price: prices[0].unit_amount, // in CHF
+    sex: sex,
+    sizes: prices.reduce(
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (acc: Record<string, SizeProduct>, price: any) => {
+        acc[price.metadata.Size] = {
+          stripeProductId: price.id,
+        };
+        return acc;
+      },
+      {},
+    ),
+  };
+}
+
+async function createProductObjects() {
+  const products = await getProducts();
+  const acc: Record<string, any> = {};
+
+  for (const product of products) {
+    const allPrices = await getPrices(product.id);
+    const sexPrices = mapSexToPrices(allPrices);
+
+    const sexPricesObjects = await Promise.all(
+      Object.keys(sexPrices).map(async (sex) => {
+        return await createProductObject(product, sex, sexPrices[sex]);
+      }),
+    );
+
+    for (const p of sexPricesObjects) {
+      const id = p.id;
+      acc[id] = p;
+    }
+
+    console.log('🪚 acc:', acc);
+  }
+
+  return acc;
+}
+
+async function writeProductsToFile(
+  filePath: string,
+  products: Record<string, any>,
+) {
+  const importDate = new Date().toISOString();
+  const fileContent = `
+// This file is auto-generated. Do not edit manually.
+// Generated on: ${importDate}
+import type { Product } from '@content/products';
+export const importedProducts: Record<string, Product> = ${JSON.stringify(products, null, 2)};\n
+`;
+
+  if (fs.existsSync(filePath)) {
+    fs.unlinkSync(filePath);
+  }
+
+  fs.writeFileSync('src/content/importedProducts.ts', fileContent, 'utf8');
+}
+
+async function main() {
+  console.log('Starting product creation process...');
+  const products = await createProductObjects();
+  console.log(`Created ${products.length} products successfully.`);
+  console.log('Writing products to importedProducts.ts...');
+
+  const filePath = 'src/content/importedProducts.ts';
+  await writeProductsToFile(filePath, products);
+  console.log(`Products written to ${filePath}`);
+}
+main().catch((error) => {
+  console.error('Error running the product creation script:', error);
+});
---
+++ src/content/importedProducts.ts
@@ -0,0 +1,220 @@
+
+// This file is auto-generated. Do not edit manually.
+// Generated on: 2025-08-20T13:11:48.958Z
+import type { Product } from '@content/products';
+export const importedProducts: Record<string, Product> = {
+  "prod_SnYOKBMkXTLTFV-unisex": {
+    "id": "prod_SnYOKBMkXTLTFV-unisex",
+    "title": "ATTAKK Aero Socks 25 - Clarity White",
+    "description": "Sleek. Silent. Intentional.The ATTAKK Aero Socks 25 – Clarity White are made to cut through wind and noise. With a race-tuned fit and aero sleeve, they’re built for speed — when your watts speak for you and your legs don’t blink.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfWXdwRDdoTWJjWlhJR09qWFVMbFNhV3gz00mFV9yU1w"
+    ],
+    "price": 2800,
+    "sex": "Unisex",
+    "sizes": {
+      "L": {
+        "stripeProductId": "price_1RrxIcDWqual0M5Bp7WzSTM9"
+      },
+      "M": {
+        "stripeProductId": "price_1RrxIBDWqual0M5BYb6xF7gl"
+      },
+      "S": {
+        "stripeProductId": "price_1RrxHrDWqual0M5BoXVZpgdV"
+      }
+    }
+  },
+  "prod_SnYHhrveP2IQoA-unisex": {
+    "id": "prod_SnYHhrveP2IQoA-unisex",
+    "title": "ATTAKK Socks 25 - Clarity White",
+    "description": "Clean. Light. Focused. The ATTAKK Socks – Clarity White strip it down to the essentials. Made for days when your effort is precise and your mindset is clear. Breathable, race-ready, and sharp enough to finish any kit. No noise. Just purpose.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfbVEyMzdxN1dhQ0VGQzhTdlJMbjgyRTRY00mv8w5xt8"
+    ],
+    "price": 1350,
+    "sex": "Unisex",
+    "sizes": {
+      "L - XL": {
+        "stripeProductId": "price_1RrxCfDWqual0M5BhrZrJynb"
+      },
+      "XS": {
+        "stripeProductId": "price_1RrxBrDWqual0M5BG0DcDyFO"
+      },
+      "S - M": {
+        "stripeProductId": "price_1RrxBIDWqual0M5B1aGE4O2w"
+      }
+    }
+  },
+  "prod_SnYEYLnprmArZ1-unisex": {
+    "id": "prod_SnYEYLnprmArZ1-unisex",
+    "title": "ATTAKK Vest 25 - Apex Blue",
+    "description": "Sharp. Silent. Ready. The ATTAKK Apex Blue Vest is built for cold starts, late moves, and days when layers matter. Lightweight, wind-cutting and race-fit, it’s your shield when conditions turn – and your edge when the game gets tactical.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfYk9hY1NkWjk2UmROUjh0VlZaUE9iWkRS008Z5hYel3"
+    ],
+    "price": 6100,
+    "sex": "Unisex",
+    "sizes": {
+      "XL": {
+        "stripeProductId": "price_1Rrx8dDWqual0M5Bu61jYWAi"
+      },
+      "L": {
+        "stripeProductId": "price_1Rrx8RDWqual0M5BjjfKXTFv"
+      },
+      "M": {
+        "stripeProductId": "price_1Rrx8IDWqual0M5BoTbdYwzB"
+      },
+      "S": {
+        "stripeProductId": "price_1Rrx7qDWqual0M5BBaFK8CC0"
+      }
+    }
+  },
+  "prod_SnBd0mH8WE0TAN-female": {
+    "id": "prod_SnBd0mH8WE0TAN-female",
+    "title": "ATTAKK Bibs 25 - Invincible Yellow",
+    "description": "Quiet. Precise. Committed. The Invincible Bibs are built for focus – from zone 4 intervals to Sunday base rides. Supportive, sharp and stripped of distraction, they’re made to match your mindset: ride hard, ride long, ride clean.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfZG5ZdmRqa1NqM3NwV0ZycThhTmlxU0Jw00G1X7mUyc"
+    ],
+    "price": 10500,
+    "sex": "Female",
+    "sizes": {
+      "XS": {
+        "stripeProductId": "price_1RyAJFDWqual0M5BNRA81Qpo"
+      },
+      "S": {
+        "stripeProductId": "price_1RyAIiDWqual0M5BpTQHTzI1"
+      },
+      "M": {
+        "stripeProductId": "price_1RyAIBDWqual0M5BcBU8XI26"
+      },
+      "L": {
+        "stripeProductId": "price_1RyAHFDWqual0M5BR5DOuSha"
+      }
+    }
+  },
+  "prod_SnBd0mH8WE0TAN-male": {
+    "id": "prod_SnBd0mH8WE0TAN-male",
+    "title": "ATTAKK Bibs 25 - Invincible Yellow",
+    "description": "Quiet. Precise. Committed. The Invincible Bibs are built for focus – from zone 4 intervals to Sunday base rides. Supportive, sharp and stripped of distraction, they’re made to match your mindset: ride hard, ride long, ride clean.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfZG5ZdmRqa1NqM3NwV0ZycThhTmlxU0Jw00G1X7mUyc"
+    ],
+    "price": 10500,
+    "sex": "Male",
+    "sizes": {
+      "XL": {
+        "stripeProductId": "price_1RrwvnDWqual0M5BdcoKiAFa"
+      },
+      "L": {
+        "stripeProductId": "price_1RrwvbDWqual0M5BlthgseQT"
+      },
+      "M": {
+        "stripeProductId": "price_1RrwvTDWqual0M5Bfym96yl0"
+      },
+      "S": {
+        "stripeProductId": "price_1RrbGDDWqual0M5BD22C0u21"
+      }
+    }
+  },
+  "prod_SnBdFNjPSXbZJk-female": {
+    "id": "prod_SnBdFNjPSXbZJk-female",
+    "title": "ATTAKK Jersey 25 - Invincible Yellow",
+    "description": "Sharp. Driven. Effortless. The Invincible Yellow Trikot is made for chasing local KOMs, pushing through intervals, and still showing up clean for the coffee stop. It’s not about race days – it’s about bringing intent to every ride, no matter the pace.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfSUM4Zm8xdFFpUWVDdjdaZFpFNmJJSFlV00CEViuJkU"
+    ],
+    "price": 7500,
+    "sex": "Female",
+    "sizes": {
+      "XS": {
+        "stripeProductId": "price_1RyAQGDWqual0M5Bm9rRzENS"
+      },
+      "S": {
+        "stripeProductId": "price_1RyAPkDWqual0M5BS3ljpMBP"
+      },
+      "M": {
+        "stripeProductId": "price_1RyAP0DWqual0M5B1BqBjLok"
+      },
+      "L": {
+        "stripeProductId": "price_1RyAOgDWqual0M5BjZKCCEF1"
+      }
+    }
+  },
+  "prod_SnBdFNjPSXbZJk-male": {
+    "id": "prod_SnBdFNjPSXbZJk-male",
+    "title": "ATTAKK Jersey 25 - Invincible Yellow",
+    "description": "Sharp. Driven. Effortless. The Invincible Yellow Trikot is made for chasing local KOMs, pushing through intervals, and still showing up clean for the coffee stop. It’s not about race days – it’s about bringing intent to every ride, no matter the pace.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfSUM4Zm8xdFFpUWVDdjdaZFpFNmJJSFlV00CEViuJkU"
+    ],
+    "price": 7500,
+    "sex": "Male",
+    "sizes": {
+      "XL": {
+        "stripeProductId": "price_1RrwrGDWqual0M5Bm2rbDUUy"
+      },
+      "L": {
+        "stripeProductId": "price_1RrwqmDWqual0M5Btp1v7whe"
+      },
+      "M": {
+        "stripeProductId": "price_1RrwqTDWqual0M5BtJRPrXHM"
+      },
+      "S": {
+        "stripeProductId": "price_1RrbFqDWqual0M5B2mxtGIM1"
+      }
+    }
+  },
+  "prod_SmUaTbKJd3uWBR-female": {
+    "id": "prod_SmUaTbKJd3uWBR-female",
+    "title": "ATTAKK Race Suit 25 - Invincible Yellow",
+    "description": "Fast. Focused. Unbreakable. The RS25 – Invincible Yellow is built for those who rise when it hurts most. When the pack fades and the night sets in, we don’t fold — we attack. This isn’t just a suit. It’s a mindset.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfU0lOUVhnazFmSFVZVzhrQU9VdjRaWXBp00qPDXgrEP"
+    ],
+    "price": 14300,
+    "sex": "Female",
+    "sizes": {
+      "L": {
+        "stripeProductId": "price_1RsItzDWqual0M5ByD1gvPv1"
+      }
+    }
+  },
+  "prod_SmUaTbKJd3uWBR-unisex": {
+    "id": "prod_SmUaTbKJd3uWBR-unisex",
+    "title": "ATTAKK Race Suit 25 - Invincible Yellow",
+    "description": "Fast. Focused. Unbreakable. The RS25 – Invincible Yellow is built for those who rise when it hurts most. When the pack fades and the night sets in, we don’t fold — we attack. This isn’t just a suit. It’s a mindset.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfU0lOUVhnazFmSFVZVzhrQU9VdjRaWXBp00qPDXgrEP"
+    ],
+    "price": 14300,
+    "sex": "Unisex",
+    "sizes": {
+      "undefined": {
+        "stripeProductId": "price_1RsIqlDWqual0M5B3GcNsrLh"
+      },
+      "L": {
+        "stripeProductId": "price_1RrbasDWqual0M5Be5XOzd3f"
+      },
+      "S": {
+        "stripeProductId": "price_1RrbaFDWqual0M5BJGdzHsoJ"
+      }
+    }
+  },
+  "prod_SmUaTbKJd3uWBR-male": {
+    "id": "prod_SmUaTbKJd3uWBR-male",
+    "title": "ATTAKK Race Suit 25 - Invincible Yellow",
+    "description": "Fast. Focused. Unbreakable. The RS25 – Invincible Yellow is built for those who rise when it hurts most. When the pack fades and the night sets in, we don’t fold — we attack. This isn’t just a suit. It’s a mindset.",
+    "imageUrls": [
+      "https://files.stripe.com/links/MDB8YWNjdF8xUnF2VjZEV3F1YWwwTTVCfGZsX3Rlc3RfU0lOUVhnazFmSFVZVzhrQU9VdjRaWXBp00qPDXgrEP"
+    ],
+    "price": 14300,
+    "sex": "Male",
+    "sizes": {
+      "M": {
+        "stripeProductId": "price_1Rqvb0DWqual0M5BXfbHQqgE"
+      }
+    }
+  }
+};
+
