554152192e367e63cfbe3c37278a889bdfcf1dab
Julian Visser <12615757+justmejulian@users.noreply.github.coml>
2026-01-19 22:10:27 +0100

    correct size order

--- src/components/SizeButtons.tsx
+++ src/components/SizeButtons.tsx
@@ -5,13 +5,21 @@ interface Props {
   sizes: string[];
 }
 
+const SIZE_ORDER = ['2xs', 'xs-s', 'xs', 's', 'm', 'l', 'l-xl', 'xl', '2xl'];
+
 export default function SizeButtons({ id, sizes }: Props) {
   const $selectedSize = useStore(selectedSize);
+  const sortedSizes = [...sizes].sort((a, b) => {
+    const aIndex = SIZE_ORDER.indexOf(a.toLowerCase());
+    const bIndex = SIZE_ORDER.indexOf(b.toLowerCase());
+    return aIndex - bIndex;
+  });
+
   return (
     <fieldset class="mb-3 flex gap-1">
       <legend class="sr-only">Delivery</legend>
 
-      {sizes.map((size) => {
+      {sortedSizes.map((size) => {
         const key = `Size-${size}-${id}`;
         return (
           <div class="flex items-center" key={size}>
